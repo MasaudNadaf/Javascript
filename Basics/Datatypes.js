0791e7f3bb14d17e323c4c72e60ec8367e423f2c
MasaudNadaf <masaudnadaf930@gmail.com>
2025-04-24 19:27:10 +0000

    till Dtatatypes and variable

---
+++ Basics/Datatypes.js
@@ -0,0 +1,23 @@
+"use strict"  //use all JS code as newer version
+
+//premitive DataType
+
+//number
+//bigInt
+//String
+//Boolean=>>> true/false
+//Null 
+//undefined
+//Symbol
+
+//NonPremitive
+//Object
+
+console.log(typeof "masaud")
+console.log(typeof null);  //Object
+console.log(typeof undefined) //undefined
+console.log(typeof 18)  //number
+const as=1;
+console.log(typeof as) //number
+
+
---
+++ Basics/Variables.js
@@ -0,0 +1,22 @@
+const custID=12345
+let custMail="gmail.com"
+var custPass="masaud123"
+custState="Maha"
+let custcity
+
+//let custMail="email.com"  cant declare again
+var custPass="Masaud"   //var can declare again
+custMail="email"      //let can update again
+
+
+custPass="Naff"     //var can update
+
+//const custID=""   cant declare again
+//const custID="M"   cant update again
+
+custState="karnatak"  //can update again without keywords
+
+
+
+
+console.table([custID,custMail,custPass,custState,custcity])
